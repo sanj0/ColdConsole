aaf90d982c493f760240289ab9bf74cee6c848f8
edgelord <malte.dostal@gmail.com>
2019-04-03 07:24:14 +0200

    Tried adding args system, not working currently

--- coldconsole.py
+++ coldconsole.py
@@ -1,53 +1,61 @@
-import os,platform,time
+# coding=utf-8
+import os, platform, time
 
 ostype = platform.system()
 SYSdrive = "DRIVE-E.VD"
 protectedFiles = [SYSdrive]
 commands = {
-"read":"read()",
-"help":"for i in commands.keys():print(i)",
-"createdisk":"i=input('Disk: ');createdisk(i);leave()",
-"disks":"for drive in drives: print(drive)",
-"sysinfo":"sysinfo()",
-"CUN":"i = input('Name: ');CUN(i)",
-"mkfile":"mkfile()",
-"ls":"ls()",
-"del":"DEL()",
-"clear":"clear()",
-"shutdown":"leave();exit()",
-"reboot":"leave();Boot()",
-"man":"i = input('Command: ');man(i)"
+    "read": "read()",
+    "help": "for i in commands.keys():print(i)",
+    "createdisk": "i=input('Disk: ');createdisk(i);leave()",
+    "disks": "for drive in drives: print(drive)",
+    "sysinfo": "sysinfo()",
+    "CUN": "i = input('Name: ');CUN(i)",
+    "mkfile": "mkfile()",
+    "ls": "ls()",
+    "del": "DEL()",
+    "clear": "clear()",
+    "shutdown": "leave();exit()",
+    "reboot": "leave();Boot()",
+    "man": "i = input('Command: ');man(i)"
 }
 
 # short descriptions for the commands, you can access them using "man"
 manuals = {
-"read":"reads out the contents of a file on a disk.",
-"help":"lists all commands.",
-"createdisk":"creates a new virtual disk.",
-"disks":"prints out the names of all disks.",
-"sysinfo":"print out system info.",
-"CUN":"renames the user.",
-"mkfile":"creates a new file.",
-"ls":"lists files of a disk",
-"del":"deletes a file.",
-"clear":"clears the screen.",
-"shutdown":"shuts ColdConsole down.",
-"reboot":"reboots ColdConsole.",
+    "read": "reads out the contents of a file on a disk. \n usage: read -d DISK -f FILE",
+    "help": "lists all commands.",
+    "createdisk": "creates a new virtual disk.",
+    "disks": "prints out the names of all disks.",
+    "sysinfo": "print out system info.",
+    "CUN": "renames the user.",
+    "mkfile": "creates a new file.",
+    "ls": "lists files of a disk",
+    "del": "deletes a file.",
+    "clear": "clears the screen.",
+    "shutdown": "shuts ColdConsole down.",
+    "reboot": "reboots ColdConsole.",
 }
 
+# the last args that the user gave
+args = []
+
+
 def read():
-    i = input("Disk: ")
+    i = get_arg_value("-d")
+    print(i)
     if i in drives:
         i += ".VD"
-        j = input("Filename: ")
+        j = get_arg_value("-f")
+        print(j)
 
-        with open(i,"r") as f:
-            exec("ddata="+f.read(),globals())
+        with open(i, "r") as f:
+            exec ("ddata=" + f.read(), globals())
             f.close()
         print(ddata[j])
 
     else:
-        print("Disk not found!")
+        print("Disk" + i + " not found!")
+
 
 def mkfile():
     i = input("Disk: ")
@@ -62,29 +70,32 @@ def mkfile():
         if j == 'exit' or data == 'exit':
             pass
 
-        with open(i,"r") as f:
-            exec("ddata="+f.read(),globals())
+        with open(i, "r") as f:
+            exec ("ddata=" + f.read(), globals())
             f.close()
 
-        ddata[j]=data
+        ddata[j] = data
 
-        with open(i,"w") as f:
+        with open(i, "w") as f:
             f.write(str(ddata))
             f.close()
     else:
         print("Disk not found!")
 
+
 def ls():
     i = input("Disk: ")
-    with open(i+".VD","r") as f:
-        exec("for i in {}.keys(): print(i)".format(f.read()))
+    with open(i + ".VD", "r") as f:
+        exec ("for i in {}.keys(): print(i)".format(f.read()))
         f.close()
+
 
 def clear():
     if ostype != "Windows":
         os.system("clear")
     else:
         os.system("cls")
+
 
 def Boot():
     try:
@@ -123,30 +134,31 @@ def coldrescue():
         cmd = input("COLDRESCUE$ ")
         if cmd == "init":
             i = input("Username: ")
-            with open(SYSdrive,"w") as f:
+            with open(SYSdrive, "w") as f:
                 f.write("['{}'];drives = ['']".format(i))
                 f.close()
         elif cmd == "read":
-            with open(SYSdrive,"r") as f:
+            with open(SYSdrive, "r") as f:
                 print(f.read())
                 f.close()
         elif cmd == "write":
             i = input("Data: ")
-            with open(SYSdrive,"w") as f:
+            with open(SYSdrive, "w") as f:
                 f.write(i)
                 f.close()
         elif cmd == "reboot":
             Boot()
         else:
-            print("Can't find command: "+cmd)
+            print("Can't find command: " + cmd)
+
 
 def OS():
     global drives
     global SYSData
     try:
         try:
-            with open(SYSdrive,"r") as f:
-                exec("SYSData = {}".format(f.read()),globals())
+            with open(SYSdrive, "r") as f:
+                exec ("SYSData = {}".format(f.read()), globals())
                 f.close()
         except:
             clear()
@@ -181,16 +193,19 @@ def OS():
         print(str(e))
         coldrescue()
 
+
 def createdisk(i):
-    if not i.replace(" ","") == "":
+    if not i.replace(" ", "") == "":
         if not i in drives:
             drives.append(i)
-        with open(i+".VD","w") as f:
+        with open(i + ".VD", "w") as f:
             f.write("{'diskinfo.tmp':''}")
             f.close()
 
+
 def sysinfo():
-    print("Processor: "+platform.processor())
+    print("Processor: " + platform.processor())
+
 
 def DEL():
     i = input("Disk: ")
@@ -199,42 +214,62 @@ def DEL():
         i += ".VD"
         j = input("Filename: ")
 
-        with open(i,"r") as f:
-            exec("ddata="+f.read(),globals())
+        with open(i, "r") as f:
+            exec ("ddata=" + f.read(), globals())
             f.close()
 
         del ddata[j]
 
-        with open(i,"w") as f:
+        with open(i, "w") as f:
             f.write(str(ddata))
             f.close()
     else:
         print("Disk not found!")
 
+
 def CUN(i):
     if not i == "":
         SYSData[0] = i
-        with open(SYSdrive,"w") as f:
-            f.write(str(SYSData)+";"+str(drives))
+        with open(SYSdrive, "w") as f:
+            f.write(str(SYSData) + ";" + str(drives))
             f.close()
     else:
         print("That is not a valid username.");
 
+
 def leave():
-    with open(SYSdrive,"w") as f:
-        f.write(str(SYSData)+";drives="+str(drives))
+    with open(SYSdrive, "w") as f:
+        f.write(str(SYSData) + ";drives=" + str(drives))
         f.close()
 
+
 def commandparser(cmd):
-    cmd = cmd.replace(" ", "");
+    args.clear()
+
     if not cmd == "":
+        # get the different part of the commands by splitting it on every space char
+        cmdParts = cmd.split()
+        # the actual command is the first entity in the array
+        command = cmdParts[0]
+
+        # make sure that the actualy command is not taken as an argument
+        index = 0
+
+        for part in cmdParts:
+            index += 1
+            if index != 1:
+                continue
+            else:
+                args.append(cmdParts[index - 1])
+
         for Key in commands.keys():
-            if cmd == Key:
-                exec(commands[Key])
+            if command == Key:
+                exec (commands[Key])
                 return True
                 break
         else:
-            print("Can't find command: "+cmd)
+            print("Can't find command: " + command)
+
 
 def man(command):
     command = command.replace(" ", "");
@@ -246,5 +281,17 @@ def man(command):
                 break
         else:
             print("Can't find command: " + command)
+
+
+# returns the value of the given key-arg.
+def get_arg_value(key):
+    key_passed = False
+    for arg in args:
+        print("Checking " + arg)
+        if key_passed:
+            return arg
+        elif arg == key:
+            key_passed = True
+
 
 Boot()
